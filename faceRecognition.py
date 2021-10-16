d53c7a371285a9409b243b48fb1f9b7c9624d066
shashank-suresh <shashanksuresh3@gmail.com>
2021-10-16 21:03:23 +0530

    Cleaned up faceRecognition.py

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,19 +1,3 @@
-# import cv2
-# import face_recognition
-
-# img = cv2.imread("dataset/00000.png")
-# rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# img_encoding = face_recognition.face_encodings(rgb_img)[0]
-
-# img2 = cv2.imread("dataset/00011.png")
-# rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-# img_encoding2 = face_recognition.face_encodings(rgb_img2)[0]
-
-# result = face_recognition.compare_faces([img_encoding], img_encoding2)
-# print(result)
-
-# cv2.waitKey(0)
-
 import cv2
 from simple_facerec import SimpleFacerec
 
